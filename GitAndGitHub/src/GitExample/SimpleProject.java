c7f4a9611832ecc0aa8e7da5c30a216b41d70b24
Kumararaja-Muthukumarasamy <reach.mdkumar@gmail.com>
2023-01-08 16:17:06 +0530

    First Commit

---
+++ GitAndGitHub/src/GitExample/SimpleProject.java
@@ -0,0 +1,14 @@
+package GitExample;
+
+public class SimpleProject {
+
+	public static void main(String[] args) {
+		boolean option=false;
+		if (option==true) 
+			System.out.println("You can add the project to Git");
+		else {
+			System.out.println("You can not add the project to Git");
+		}
+	}
+
+}
